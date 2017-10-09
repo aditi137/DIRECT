18657542dff7a4ba7e0a63572628dcfbffdc8788
aditi137 <aditidutta137@gmail.com>
2017-10-10 01:56:40 +0800

    improvised function calls from main

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,8 @@
 from direct.direct import Direct
 from problem.helper import *
 
-def result(d):
-    curr_opt, x_at_opt, l_hist = d.run()
+def result(f, bounds):
+    curr_opt, x_at_opt, l_hist = Direct(f,bounds).run()
     print("curr_opt =", curr_opt, ",  x_at_opt =", x_at_opt)
     print()
 
@@ -10,25 +10,20 @@ def result(d):
 if __name__ == "__main__":
     print('test Goldstein-Price results:')
 #    x=np.array([2,2])
-    d = Direct(func1, bounds=np.array([[-2,2,],[-2,2,]]))
-    result(d)
+    result(func1, bounds=np.array([[-2,2,],[-2,2,]]))
   
     print('test Rosenbrock results:')
 #    x = np.array([-2.,7.])
-    d = Direct(func2, bounds=np.array([[-5,5],[-2,8]]))
-    result(d)
+    result(func2, bounds=np.array([[-5,5],[-2,8]]))
    
     print('test Six-hump Camelback results:')
 #    x = np.array([-1.,1.])
-    d = Direct(func3, bounds=np.array([[-5,5],[-5,5]]))
-    result(d)
+    result(func3, bounds=np.array([[-5,5],[-5,5]]))
      
     print('test Rastrigin results:')
 #    x = np.array([-1.,1.])
-    d = Direct(func4, bounds=np.array([[-1,1],[-1,1]]))
-    result(d)
+    result(func4, bounds=np.array([[-1,1],[-1,1]]))
  
     print('test Griewank results:')
 #    x = np.zeros(2)
-    d = Direct(func5, bounds=np.array([[-600,600],[-600,600]]))
-    result(d)
+    result(func5, bounds=np.array([[-600,600],[-600,600]]))
