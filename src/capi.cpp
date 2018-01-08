d11d87417da52c9d6a01587662ec0f4626cc1e2b
aditi137 <15117361+aditi137@users.noreply.github.com>
2018-01-08 18:29:55 +0800

    cp Hilbert\Hilbert\*

---
+++ src/capi.cpp
@@ -0,0 +1,39 @@
+//TODO: organize all definitions in capi.cpp, hilbert.cpp, hilbert.h
+//TODO: add py-c implementation and exception handling for remaining functions
+#include "hilbert.cpp"
+#include <iostream>
+
+using namespace std;
+
+
+static PyObject* some_func(PyObject* self, PyObject* args)
+{
+	__int64 input_value;
+	if (!PyArg_ParseTuple(args, "L", &input_value))
+		return 0;
+	return PyLong_FromLongLong(input_value + 1);
+}
+
+
+PyMODINIT_FUNC PyInit_Hilbert(void)
+{
+	static PyMethodDef HilbertMethods[] = {
+		{ "add_one", some_func, METH_VARARGS , NULL },
+		{ "t2r", TransposetoAxes, METH_VARARGS, "Transpose to Real" },
+		{ "r2t", AxestoTranspose, METH_VARARGS, "Real to Transpose" },
+		{ NULL, NULL, 0/* flag telling the interpreter the calling convention to be used for the C function */, NULL }		/* Sentinel */
+	};
+
+	static struct PyModuleDef hilbertmodule = {
+		PyModuleDef_HEAD_INIT,
+		"Hilbert",	/* name of module */
+		NULL,		/* module documentation, may be NULL */
+		-1,			/* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+		HilbertMethods	/* array of exported functions */
+	};
+
+	PyObject *m = PyModule_Create(&hilbertmodule);
+	if (m == NULL)
+		return NULL;
+	return m;
+}
--- src/hilbert.cpp
+++ src/hilbert.cpp
@@ -1,105 +1,120 @@
+//TODO: Unhandled exception, most probably a memory leak somewhere
 #include "hilbert.h"
-#include <iostream>
 
-
+/*
 void LinetoAxes (
-	coord_t* Axes,	// multidimensional geometrical axes
-	coord_t* Line,	// linear serial # 
-	int b,			// # bits
-	int n)			// dimension
+coord_t* Axes,	// multidimensional geometrical axes
+coord_t* Line,	// linear serial #
+int b,			// # bits
+int n)			// dimension
 {
-	if (n <= 1)		// trivial case
-		*Axes = *Line;
-	else {
-		LinetoTranspose(Axes, Line, b, n);
-		TransposetoAxes(Axes, b, n);
-	}
+if (n <= 1)		// trivial case
+*Axes = *Line;
+else {
+LinetoTranspose(Axes, Line, b, n);
+TransposetoAxes(Axes, b, n);
+}
 }
 
 
 void AxestoLine (
-	coord_t* Line,	// linear serial # 
-	coord_t* Axes,	// multidimensional geometrical axes
-	int b,			// # bits
-	int n)			// dimension
+coord_t* Line,	// linear serial #
+coord_t* Axes,	// multidimensional geometrical axes
+int b,			// # bits
+int n)			// dimension
 {
-	coord_t store[1024];	// avoid overwriting Axes
-	int i;
+coord_t store[1024];	// avoid overwriting Axes
+int i;
 
-	// trivial case
-	if (n <= 1)
-		*Line = *Axes;
-	// surely the usual case
-	else if (n <= 1024) {
-		for (i = 0; i < n; i++)
-			store[i] = Axes[i];
-		AxestoTranspose(store, b, n);
-		TransposetoLine(Line, store, b, n);
-	}
-	// must do in place at greater cost
-	else {
-		AxestoTranspose(Axes, b, n);
-		TransposetoLine(Line, Axes, b, n);
-		TransposetoAxes(Axes, b, n);
-	}
+// trivial case
+if (n <= 1)
+*Line = *Axes;
+// surely the usual case
+else if (n <= 1024) {
+for (i = 0; i < n; i++)
+store[i] = Axes[i];
+AxestoTranspose(store, b, n);
+TransposetoLine(Line, store, b, n);
+}
+// must do in place at greater cost
+else {
+AxestoTranspose(Axes, b, n);
+TransposetoLine(Line, Axes, b, n);
+TransposetoAxes(Axes, b, n);
+}
 }
 
 
 static void LinetoTranspose (
-	coord_t* X,		// transpose
-	coord_t* Line,	// Hilbert integer
-	int b,			// # bits
-	int n)			// dimension
+coord_t* X,		// transpose
+coord_t* Line,	// Hilbert integer
+int b,			// # bits
+int n)			// dimension
 {
-	coord_t M = 1 << (b - 1), p, q;
-	int i, j = 0;
-	p = M;
-	for (i = 0; i < n; i++)
-		X[i] = 0;
-	for (i = 0; i < n; i++)
-		for (q = M; q > 0 ; q >>= 1) {
-			if (Line[i] & q)
-				X[j] |= p;
-			if (++j == n) {
-				j = 0;
-				p >>= 1;
-			}
-		}
+coord_t M = 1 << (b - 1), p, q;
+int i, j = 0;
+p = M;
+for (i = 0; i < n; i++)
+X[i] = 0;
+for (i = 0; i < n; i++)
+for (q = M; q > 0 ; q >>= 1) {
+if (Line[i] & q)
+X[j] |= p;
+if (++j == n) {
+j = 0;
+p >>= 1;
+}
+}
 }
 
 
 static void TransposetoLine(
-	coord_t* Line,	// Hilbert integer 
-	coord_t* X,		// transpose
-	int b,			// # bits
-	int n)			// dimension
+coord_t* Line,	// Hilbert integer
+coord_t* X,		// transpose
+int b,			// # bits
+int n)			// dimension
 {
-	coord_t M = 1 << (b - 1), p, q;
-	int i, j = 0;
-	p = M;
+coord_t M = 1 << (b - 1), p, q;
+int i, j = 0;
+p = M;
 
-	for (i = 0; i < n; i++) {
-		Line[i] = 0;
-		for (q = M; q > 0; q >>= 1) {
-			if (X[j] & p)
-				Line[i] |= q;
-			if (++j == n) {
-				j = 0;
-				p >>= 1;
-			}
-		}
-	}
+for (i = 0; i < n; i++) {
+Line[i] = 0;
+for (q = M; q > 0; q >>= 1) {
+if (X[j] & p)
+Line[i] |= q;
+if (++j == n) {
+j = 0;
+p >>= 1;
 }
+}
+}
+}
+*/
 
-
-static void TransposetoAxes(
-	coord_t* X,	// position
-	int b,		// #bits
-	int n)		// dimension
+static PyObject* TransposetoAxes(PyObject* self, PyObject* args)
 {
-	coord_t M = 2 << (b - 1), p, q, t;
-	int i;
+	coord_t* X = new coord_t;	// position
+	int b, n, i;				// # bits, dimension, counter
+	PyObject *pX, *pItem;
 
+	// assigning python input to c++ types
+	if (!PyArg_ParseTuple(args, "iiO!", &b, &n, &PyList_Type, &pX)) {
+		PyErr_SetString(PyExc_TypeError, "Parameters must be an int, an int, and a list.");
+		return NULL;
+	}
+
+	for (i = 0; i < PyList_Size(pX); i++) {
+		pItem = PyList_GetItem(pX, i);
+		if (!PyLong_Check(pItem)) {
+			PyErr_SetString(PyExc_TypeError, "List items must be integers.");
+			return NULL;
+		}
+		X[i] = PyLong_AsUnsignedLong(pItem);
+	}
+
+	// TransposetoAxes ...
+	coord_t M = 2 << (b - 1), p, q, t;
 	// Gray decode by H ^ (H/2)
 	t = X[n - 1] >> 1;
 	for (i = n - 1; i > 0; i--)
@@ -121,16 +136,39 @@ static void TransposetoAxes(
 			}
 		}
 	}
+
+	// return X as python list
+	for (i = 0; i < PyList_Size(pX); i++) {
+		PyList_SetItem(pX, i, PyLong_FromLong(X[i]));
+	}
+	delete X;
+	return pX;
 }
 
 
-static void AxestoTranspose(
-	coord_t* X,	// position
-	int b,		// # bits
-	int n)		// dimension
+static PyObject* AxestoTranspose(PyObject* self, PyObject* args)
 {
+	coord_t* X = new coord_t;	// position
+	int b, n, i;				// # bits, dimension, counter
+	PyObject *pX, *pItem;
+
+	// assigning python input to c++ types
+	if (!PyArg_ParseTuple(args, "iiO!", &b, &n, &PyList_Type, &pX)) {
+		PyErr_SetString(PyExc_TypeError, "Parameters must be an int, an int, and a list.");
+		return NULL;
+	}
+
+	for (i = 0; i < PyList_Size(pX); i++) {
+		pItem = PyList_GetItem(pX, i);
+		if (!PyLong_Check(pItem)) {
+			PyErr_SetString(PyExc_TypeError, "List items must be integers.");
+			return NULL;
+		}
+		X[i] = PyLong_AsUnsignedLong(pItem);
+	}
+
+	// AxestoTranspose ...
 	coord_t M = 1 << (b - 1), p, q, t;
-	int i;
 	// Inverse undo
 	for (q = M; q > 1; q >>= 1) {
 		p = q - 1;
@@ -151,21 +189,15 @@ static void AxestoTranspose(
 		X[i] ^= X[i - 1];
 	t = 0;
 	for (q = M; q > 1; q >>= 1)
-		if (X[n - 1] & q)
-			t ^= q - 1;
+	if (X[n - 1] & q)
+		t ^= q - 1;
 	for (i = 0; i < n; i++)
 		X[i] ^= t;
-}
 
-//
-//// entry point for Hilbert.cpp
-//void main() {
-//	coord_t X[3] = {5, 10, 20};	// any position in 32x32x32 cube
-//	AxestoTranspose(X, 5, 3);	// Hilbert transpose for 5 bits and 3 dimensions
-//	std::cout << "Hilbert integer = ";	// check = 7865 or 001111010111001
-//	std::cout << (X[0] >> 4 & 1) << (X[1] >> 4 & 1) << (X[2] >> 4 & 1) << (X[0] >> 3 & 1) << (X[1] >> 3 & 1);
-//	std::cout << (X[2] >> 3 & 1) << (X[0] >> 2 & 1) << (X[1] >> 2 & 1) << (X[2] >> 2 & 1) << (X[0] >> 1 & 1),
-//	std::cout << (X[1] >> 1 & 1) << (X[2] >> 1 & 1) << (X[0] >> 0 & 1) << (X[1] >> 0 & 1) << (X[2] >> 0 & 1);
-//	
-//	std::cin.get();
-//}
+	// return X as python list
+	for (i = 0; i < PyList_Size(pX); i++) {
+		PyList_SetItem(pX, i, PyLong_FromLong(X[i]));
+	}
+	delete X;
+	return pX;
+}
