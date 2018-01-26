35bcf7276c5e607bbe898ee4016c0ad6363703be
aditi137 <15117361+aditi137@users.noreply.github.com>
2018-01-26 20:06:25 +0800

    move to pytest

--- test_Hilbert.py
+++ test_Hilbert.py
@@ -1,11 +1,9 @@
 import Hilbert
-assert dir(Hilbert) == ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'h2r', 'r2h']
 
-try:
+def test_Hilbert_import():
+    assert dir(Hilbert) == ['__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'h2r', 'r2h']
+
+def test_Hilbert_curve():
     X = Hilbert.r2h(5, 3, [5, 10, 20])
     assert X == [7, 21, 25]
     assert Hilbert.h2r(5, 3, X) == [5, 10, 20]
-    print("ok.")
-    
-except Exception as e:
-    print(e)
